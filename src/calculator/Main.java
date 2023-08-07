2b455056080ea18c41161751cfac951e020a8892
Uneld <aleksey.minchenya@gmail.com>
2023-08-07 22:06:50 +0300

    rename main and change path log file

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,7 +1,7 @@
 package calculator;
 import calculator.app.CalculatorApp;
 
-public class MainT1 {
+public class Main {
     public static void main(String[] args) {
         CalculatorApp calculatorApp = new CalculatorApp();
         calculatorApp.start();
